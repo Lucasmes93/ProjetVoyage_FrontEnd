75955db103067e3f9212d98fe358512c7d2c6562
Lucas MESSIA <lmessia93@gmail.com>
2023-10-18 11:36:49 +0200

    interface update

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {User} from "../interface/user.interface";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor() {
+  }
+
+  getUsers{}:
 }
