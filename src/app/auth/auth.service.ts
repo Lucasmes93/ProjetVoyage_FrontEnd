54cc464d524316c3198b5eb34b7e8a75954b836a
Lucas MESSIA <lmessia93@gmail.com>
2023-10-19 12:24:45 +0200

    update

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,20 +1,22 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-  private authToken: string | null = null;
+export class ApiService {
+  private apiUrl = 'http://127.0.0.1:8000/api/token/'; // Remplacez par l'URL de votre API
+  private authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3NzEwODQ4LCJpYXQiOjE2OTc3MTA1NDgsImp0aSI6ImZkYTc2NDI2NzNhZjQ4MDM5NTc0M2JkNDU4MWNhZGE5IiwidXNlcl9pZCI6MX0.S5HiE3FmYx0IsxoNOv7BvqCuraPNkrbz4sRNp85OSIs'; // Remplacez par votre token d'authentification
 
-  setToken(token: string): void {
-    this.authToken = token;
+  constructor(private http: HttpClient) {
   }
 
-  getToken(): string | null {
-    return this.authToken;
-  }
+  getData(): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.authToken}` // Utilisez le token d'authentification dans l'en-tête
+    });
 
-  isAuthenticated(): boolean {
-    return this.authToken !== null;
+    return this.http.get(this.apiUrl, {headers});
   }
 }
--- src/app/destinations/destinations-view/destinations-view.component.ts
+++ src/app/destinations/destinations-view/destinations-view.component.ts
@@ -1,33 +1,37 @@
-import { Component} from '@angular/core';
-import {Destinations} from "../../interface/destinations.interface";
-import {DestinationsService} from "../destinations.service";
-import { AuthService } from '../../auth/auth.service';
+import {Component, OnInit} from '@angular/core';
+import {Destinations} from '../../interface/destinations.interface';
+import {DestinationsService} from '../destinations.service';
+import {ApiService} from '../../auth.service'; // Assurez-vous d'importer ApiService depuis le bon emplacement
 
 @Component({
   selector: 'app-destinations-view',
   templateUrl: './destinations-view.component.html',
-  styleUrls: ['./destinations-view.component.css']
+  styleUrls: './destinations-view.component.css'
 })
-export class DestinationsViewComponent {
+export class DestinationsViewComponent implements OnInit {
   destinations: Destinations[] = [];
   displayedColumns: string[] = ['id', 'name', 'description'];
 
-  constructor(private destinationsService: DestinationsService, private authService: AuthService) {
+  constructor(
+    private destinationsService: DestinationsService,
+    private apiService: ApiService // Injection du service ApiService
+  ) {
   }
 
-  ngOnInit(): void {
-    // Récupérez le token depuis le service AuthService
-    const authToken = this.authService.getToken();
+  ngOnInit() {
+    this.appelAPIAvecToken();
+  }
 
-    // Vérifiez si un token est disponible
-    if (authToken) {
-      // Utilisez le token pour effectuer une requête authentifiée
-      this.destinationsService.getDestinations(authToken).subscribe((destinations: Destinations[]) => {
-        this.destinations = this.destinations;
-      });
-    } else {
-      console.log('Aucun token d\'authentification disponible.');
-      // Redirigez l'utilisateur vers la page de connexion ou prenez d'autres mesures en conséquence.
-    }
+  appelAPIAvecToken() {
+    this.apiService.getData().subscribe(
+      data => {
+        // Gérer la réponse ici
+        console.log(data);
+      },
+      error => {
+        // Gérer les erreurs ici
+        console.error('Erreur lors de la récupération des données :', error);
+      }
+    );
   }
 }
