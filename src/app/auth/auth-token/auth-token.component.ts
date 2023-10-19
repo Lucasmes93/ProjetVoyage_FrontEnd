46b1a295210828a010c18c801aedc416ebddcbff
Lucas MESSIA <lmessia93@gmail.com>
2023-10-19 13:32:02 +0200

    Update auth-token.component.ts

--- src/app/auth/auth-token/auth-token.component.ts
+++ src/app/auth/auth-token/auth-token.component.ts
@@ -13,7 +13,7 @@ export class AuthComponent implements OnInit {
 
   ngOnInit(): void {
     // Obtenez le token de Postman (remplacez 'YOUR_TOKEN_FROM_POSTMAN' par le vrai token)
-    const tokenFromPostman = 'YOUR_TOKEN_FROM_POSTMAN'; // Remplacez 'YOUR_TOKEN_FROM_POSTMAN' par le vrai token
+    const tokenFromPostman = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3NzEwODQ4LCJpYXQiOjE2OTc3MTA1NDgsImp0aSI6ImZkYTc2NDI2NzNhZjQ4MDM5NTc0M2JkNDU4MWNhZGE5IiwidXNlcl9pZCI6MX0.S5HiE3FmYx0IsxoNOv7BvqCuraPNkrbz4sRNp85OSIs'; // Remplacez 'YOUR_TOKEN_FROM_POSTMAN' par le vrai token
 
     // Utilisez la méthode pour définir le token
     this.authService.setTokenFromPostman(tokenFromPostman);
@@ -22,3 +22,4 @@ export class AuthComponent implements OnInit {
     // Vous pouvez maintenant rediriger l'utilisateur ou effectuer d'autres opérations ici en fonction de votre logique.
   }
 }
+
