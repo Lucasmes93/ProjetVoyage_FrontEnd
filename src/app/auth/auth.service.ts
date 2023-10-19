d6dceb20d79aea4431954546085fba6977632be7
IlesYazi <96123733+IlesYazi@users.noreply.github.com>
2023-10-19 13:56:25 +0200

    ds

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,7 +7,7 @@ import {Observable} from 'rxjs';
 })
 export class ApiService {
   private apiUrl = 'http://127.0.0.1:8000/api/token/';
-  private authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3NzE2NTEzLCJpYXQiOjE2OTc3MTYyMTMsImp0aSI6ImJlMWM3Y2M3ZjZiZTQwYWY5MzU4NzBiNTU0MDA5ODQyIiwidXNlcl9pZCI6MX0.d82fDfV1RIwPn0k0iaxE8K5PNWG6IQA1vHturEF9Sz0'; // Remplacez par votre token d'authentification
+  private authToken = 'eyJ0eXAiOidsJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3NzE2NTEzLCJpYXQiOjE2OTc3MTYyMTMsImp0aSI6ImJlMWM3Y2M3ZjZiZTQwYWY5MzU4NzBiNTU0MDA5ODQyIiwidXNlcl9pZCI6MX0.d82fDfV1RIwPn0k0iaxE8K5PNWG6IQA1vHturEF9Sz0'; // Remplacez par votre token d'authentification
 
   constructor(private http: HttpClient) {
   }
