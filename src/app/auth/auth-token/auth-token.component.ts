a71157088d3fe2fc06d661313d878bbe519af1b8
Lucas MESSIA <lmessia93@gmail.com>
2023-10-19 13:55:53 +0200

    error

--- src/app/auth/auth-token/auth-token.component.ts
+++ src/app/auth/auth-token/auth-token.component.ts
@@ -4,16 +4,17 @@ import {ApiService} from '../auth.service';
 
 @Component({
   selector: 'app-auth',
-  templateUrl: './auth-token.component.html',
-  styleUrls: ['./auth-token.component.css']
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.css']
 })
-export class AuthTokenComponent implements OnInit {
+export class AuthComponent implements OnInit {
   constructor(private route: ActivatedRoute, private authService: ApiService) {
   }
 
   ngOnInit(): void {
     // Obtenez le token de Postman (remplacez 'YOUR_TOKEN_FROM_POSTMAN' par le vrai token)
-    const tokenFromPostman = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3NzE2NTEzLCJpYXQiOjE2OTc3MTYyMTMsImp0aSI6ImJlMWM3Y2M3ZjZiZTQwYWY5MzU4NzBiNTU0MDA5ODQyIiwidXNlcl9pZCI6MX0.d82fDfV1RIwPn0k0iaxE8K5PNWG6IQA1vHturEF9Sz0'
+    const tokenFromPostman = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk3NzEwODQ4LCJpYXQiOjE2OTc3MTA1NDgsImp0aSI6ImZkYTc2NDI2NzNhZjQ4MDM5NTc0M2JkNDU4MWNhZGE5IiwidXNlcl9pZCI6MX0.S5HiE3FmYx0IsxoNOv7BvqCuraPNkrbz4sRNp85OSIs'; // Remplacez 'YOUR_TOKEN_FROM_POSTMAN' par le vrai token
+
     // Utilisez la méthode pour définir le token
     this.authService.setTokenFromPostman(tokenFromPostman);
 
