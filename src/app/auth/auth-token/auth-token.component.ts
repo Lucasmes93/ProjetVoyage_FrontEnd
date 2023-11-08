b7ffb457bb8b7b1a32718fe2e0cb667c46e815ad
Lucas MESSIA <lmessia93@gmail.com>
2023-11-08 12:52:11 +0100

    Description + Bouton Recherche par ID

--- src/app/auth/auth-token/auth-token.component.ts
+++ src/app/auth/auth-token/auth-token.component.ts
@@ -1,24 +1,25 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { ApiService } from '../auth.service';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {ApiService} from '../auth.service';
 
 @Component({
-  selector: 'app-auth',
-  templateUrl: './auth-token.component.html',
-  styleUrls: ['./auth-token.component.css'],
+    selector: 'app-auth',
+    templateUrl: './auth-token.component.html',
+    styleUrls: ['./auth-token.component.css'],
 })
 export class AuthTokenComponent implements OnInit {
-  constructor(private route: ActivatedRoute, private authService: ApiService) {}
+    constructor(private route: ActivatedRoute, private authService: ApiService) {
+    }
 
-  ngOnInit(): void {
-    // Obtenez le token de Postman (remplacez 'YOUR_TOKEN_FROM_POSTMAN' par le vrai token)
-    const tokenFromPostman =
-      'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5NDQzOTMwLCJpYXQiOjE2OTk0NDM2MzAsImp0aSI6IjVlMWRhYjYzNTk2MTQ5YzhiYzQwYTM4MzZlY2Y0ZTRmIiwidXNlcl9pZCI6MX0.-vYE682CfKpyJItq-bDS0RIuKk2ND8gwwruATrPb_Sk'; // Remplacez 'YOUR_TOKEN_FROM_POSTMAN' par le vrai token
+    ngOnInit(): void {
+        // Obtenez le token de Postman (remplacez 'YOUR_TOKEN_FROM_POSTMAN' par le vrai token)
+        const tokenFromPostman =
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5NDQ0NTYyLCJpYXQiOjE2OTk0NDQyNjIsImp0aSI6IjRhNzg1OTc4NjJiODQ4Yjc5Mzc1ZTllYjJlNjdlNDFjIiwidXNlcl9pZCI6MX0.i_GsVV0Uv5izFTPPZP93lb1m2CoLqSga6nkSHSwtLmA'; // Remplacez 'YOUR_TOKEN_FROM_POSTMAN' par le vrai token
 
-    // Utilisez la méthode pour définir le token
-    this.authService.setTokenFromPostman(tokenFromPostman);
+        // Utilisez la méthode pour définir le token
+        this.authService.setTokenFromPostman(tokenFromPostman);
 
-    console.log('Token enregistré depuis Postman : ' + tokenFromPostman);
-    // Vous pouvez maintenant rediriger l'utilisateur ou effectuer d'autres opérations ici en fonction de votre logique.
-  }
+        console.log('Token enregistré depuis Postman : ' + tokenFromPostman);
+        // Vous pouvez maintenant rediriger l'utilisateur ou effectuer d'autres opérations ici en fonction de votre logique.
+    }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,24 +3,24 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ApiService {
-  private apiUrl = 'http://127.0.0.1:8000/api/destinations/';
-  private authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5NDQzOTMwLCJpYXQiOjE2OTk0NDM2MzAsImp0aSI6IjVlMWRhYjYzNTk2MTQ5YzhiYzQwYTM4MzZlY2Y0ZTRmIiwidXNlcl9pZCI6MX0.-vYE682CfKpyJItq-bDS0RIuKk2ND8gwwruATrPb_Sk'; // Remplacez par votre token d'authentification
+    private apiUrl = 'http://127.0.0.1:8000/api/destinations/';
+    private authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5NDQ0NTYyLCJpYXQiOjE2OTk0NDQyNjIsImp0aSI6IjRhNzg1OTc4NjJiODQ4Yjc5Mzc1ZTllYjJlNjdlNDFjIiwidXNlcl9pZCI6MX0.i_GsVV0Uv5izFTPPZP93lb1m2CoLqSga6nkSHSwtLmA'; // Remplacez par votre token d'authentification
 
-  constructor(private http: HttpClient) {
-  }
+    constructor(private http: HttpClient) {
+    }
 
-  getData(): Observable<any> {
-    const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.authToken}`
-    });
+    getData(): Observable<any> {
+        const headers = new HttpHeaders({
+            'Authorization': `Bearer ${this.authToken}`
+        });
 
-    return this.http.get(this.apiUrl, {headers});
-  }
+        return this.http.get(this.apiUrl, {headers});
+    }
 
-  setTokenFromPostman(tokenFromPostman: string) {
+    setTokenFromPostman(tokenFromPostman: string) {
 
-  }
+    }
 }
