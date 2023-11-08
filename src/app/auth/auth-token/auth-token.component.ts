c9517de5f9d89dd2f5974cfa73c2eb7463e14843
Lucas MESSIA <lmessia93@gmail.com>
2023-11-08 15:11:50 +0100

    token changed

--- src/app/auth/auth-token/auth-token.component.ts
+++ src/app/auth/auth-token/auth-token.component.ts
@@ -14,7 +14,7 @@ export class AuthTokenComponent implements OnInit {
     ngOnInit(): void {
         // Obtenez le token de Postman (remplacez 'YOUR_TOKEN_FROM_POSTMAN' par le vrai token)
         const tokenFromPostman =
-            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5NDQ0NTYyLCJpYXQiOjE2OTk0NDQyNjIsImp0aSI6IjRhNzg1OTc4NjJiODQ4Yjc5Mzc1ZTllYjJlNjdlNDFjIiwidXNlcl9pZCI6MX0.i_GsVV0Uv5izFTPPZP93lb1m2CoLqSga6nkSHSwtLmA'; // Remplacez 'YOUR_TOKEN_FROM_POSTMAN' par le vrai token
+            'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5NDUwMDYxLCJpYXQiOjE2OTk0NDk3NjEsImp0aSI6IjAwNDBmMWYwNjJjMjRlMDZiMzE4MzY2MTQ1ZmZmMmVhIiwidXNlcl9pZCI6MX0.WuOSSpfMwwJc3zbECoCmkyi57ebAX5KTvcgr52zPqYA'; // Remplacez 'YOUR_TOKEN_FROM_POSTMAN' par le vrai token
 
         // Utilisez la méthode pour définir le token
         this.authService.setTokenFromPostman(tokenFromPostman);
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,7 +7,7 @@ import {Observable} from 'rxjs';
 })
 export class ApiService {
     private apiUrl = 'http://127.0.0.1:8000/api/destinations/';
-    private authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5NDQ0NTYyLCJpYXQiOjE2OTk0NDQyNjIsImp0aSI6IjRhNzg1OTc4NjJiODQ4Yjc5Mzc1ZTllYjJlNjdlNDFjIiwidXNlcl9pZCI6MX0.i_GsVV0Uv5izFTPPZP93lb1m2CoLqSga6nkSHSwtLmA'; // Remplacez par votre token d'authentification
+    private authToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjk5NDUwMDYxLCJpYXQiOjE2OTk0NDk3NjEsImp0aSI6IjAwNDBmMWYwNjJjMjRlMDZiMzE4MzY2MTQ1ZmZmMmVhIiwidXNlcl9pZCI6MX0.WuOSSpfMwwJc3zbECoCmkyi57ebAX5KTvcgr52zPqYA'; // Remplacez par votre token d'authentification
 
     constructor(private http: HttpClient) {
     }
